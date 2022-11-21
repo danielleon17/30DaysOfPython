4b12174713a36963a0a427211ef133a281edfb09
danielleon17 <113900677+danielleon17@users.noreply.github.com>
2022-11-21 12:33:41 +0100

    Add files via upload

---
+++ Day_2/variables.py
@@ -0,0 +1,53 @@
+#Day 2: 30 Days of python programming
+from colorsys import TWO_THIRD
+from os import name
+
+
+first_name = 'Daniel'    
+last_name = 'León'       
+full_name = 'Daniel León'
+country = 'España'         
+city= 'Jerez de la Frontera'            
+age = 17
+year = 2022
+is_married = 'no'
+is_true = 'Yes'
+is_light_on = 'Yes'
+print(first_name, last_name, country, age, is_married)
+
+print(type('Daniel'))
+print(type('León'))
+print(type('Daniel León'))
+print(type('España'))
+print(type('Jerez de la frontera'))
+print(type('17'))
+print(type('2022'))
+print(type('no'))
+print(type('Yes'))
+print(type('Yes'))
+
+len('Daniel')
+len('León')
+
+
+num_one = 5
+num_two =4
+total = num_one + num_two
+diff = num_one - num_two 
+product = num_one * num_two
+division = num_one / num_two
+diffremainder = num_one // num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+area_of_circle = 3.14159*30*30
+circum_of_circle = 3.14159*60
+radio = 30
+area = 3.14159*30**2
+
+built-in (first_name) 
+built-in (last_name)
+built-in (country)
+built-in (age)
+
+'keywords'
